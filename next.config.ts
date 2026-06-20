ed78b8344bc55f5aa5d077f6c23de62b580a9775
Dieggo775 <dieggo_taylor@hotmail.com>
2026-06-20 17:23:41 -0300

    Estrutura Cabeçalho

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    API_URL: "https://6a36bc78766b831960f98239.mockapi.io",
+  },
+  allowedDevOrigins: ["192.168.15.13"],
   /* config options here */
   reactCompiler: true,
 };
