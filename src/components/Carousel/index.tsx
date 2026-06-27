b25e11a06e2c289b252900436ac99451f183c483
Dieggo775 <dieggo_taylor@hotmail.com>
2026-06-27 19:03:17 -0300

    Troca personagens Carrossel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import HeroDetails from "../HeroDetails";
 
 import styles from "./carousel.module.scss";
 
 import { IHeroData } from "@/interfaces/heroes";
+import { div } from "framer-motion/client";
+import HeroPicture from "../HeroPicture";
 
 interface IProps {
   heroes: IHeroData[];
@@ -10,10 +16,48 @@ interface IProps {
 }
 
 export default function Carousel({ heroes, activeId }: IProps) {
+  const [visibleItems, setVisibleItems] = useState<IHeroData[] | null>(null);
+  const [activeIndex, setActiveIndex] = useState<number>(
+    heroes.findIndex((hero) => hero.id === activeId),
+  );
+
+  useEffect(() => {
+    const indexInArrayScope =
+      ((activeIndex % heroes.length) + heroes.length) % heroes.length;
+
+    const visibleItems = [...heroes, ...heroes].slice(
+      indexInArrayScope,
+      indexInArrayScope + 3,
+    );
+
+    setVisibleItems(visibleItems);
+  }, [heroes, activeIndex]);
+
+  // Altera heroi ativo no carrossel
+  // +1 rotaciona no sentido horário
+  // -1 rotaciona no sentido anti-horário
+  const handleChangeActiveIndex = (newDirection: number) => {
+    setActiveIndex((prevActiveIndex) => prevActiveIndex + newDirection);
+  };
+
+  if (!visibleItems) {
+    return null;
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.carousel}>
-        <div className={styles.wrapper}>Lista com Heróis</div>
+        <div
+          className={styles.wrapper}
+          onClick={() => handleChangeActiveIndex(1)}
+        >
+          {visibleItems.map((item) => (
+            <div key={item.id} className={styles.hero}>
+              <HeroPicture hero={item} />
+            </div>
+          ))}
+          ;
+        </div>
       </div>
       <div className={styles.details}>
         <HeroDetails data={heroes[0]} />
